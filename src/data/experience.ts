b654d46edf0926f69949879208058867b87c357f
Faaiz Ali <alisyedfaaiz@gmail.com>
2026-09-19 20:00:58 +0530

    Update internship details

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -6,7 +6,7 @@ export const experiences: Experience[] = [
     company: 'Codec Technologies',
     period: 'June 15, 2026 - July 14, 2026',
     location: 'Mumbai',
-    type: 'Internship',
+    type: 'Internship (Virtual)',
     description:
       'Worked as a Python Developer Intern, gaining practical experience in Python programming, application development, data processing, and problem-solving. During the internship, I worked on real-world development tasks and built Python-based projects to strengthen my understanding of software development.',
     highlights: [
